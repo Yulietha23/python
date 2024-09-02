047458b2a54608bdcf9ec8f4f43811fbc0f40c88
Yulietha23 <77504720+Yulietha23@users.noreply.github.com>
2024-09-02 12:46:20 -0500

    Create numeric-data.py

---
+++ numeric-data.py
@@ -0,0 +1,33 @@
+print("Ejemplo 1")
+print("Python has three numeric types: int, float, and complex")
+print()
+
+myValue=1
+print(myValue)
+print()
+
+print("Ejemplo 2:")
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print()
+
+myValue=3.14
+print("Ejemplo 3:")
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print()
+
+myValue=5j
+print("Ejemplo 4:")
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print()
+
+myValue=True
+print("Ejemplo 5:")
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print()
